c96f66f44c2b79653e7a6e26b041f760b0ae5cb6
Paul Sohns <paul.sohns@web.de>
2023-08-28 10:24:27 +0200

    Add needed files

---
+++ format.sh
@@ -0,0 +1,1 @@
+find ./src -name '*.h' -o -name '*.cpp' | xargs clang-format -i --verbose --style=file:.clang-format
---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include "src/AppleTree.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hi" << std::endl;
+}
